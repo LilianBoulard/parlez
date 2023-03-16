a64e0fe8ae862b526ad72a9c5c6896f787cb22d2
Lilian <lilian@boulard.fr>
2023-03-16 12:54:53 +0100

    Symlink whole lib

--- setup_tortoise.sh
+++ setup_tortoise.sh
@@ -8,9 +8,4 @@ pip install --upgrade --disable-pip-version-check --no-input -r requirements.txt
 python setup.py install
 # Create symbolic links for the Discord bot to have access to the tortoise API
 cd ../robot/
-mkdir tortoise
-cd tortoise
-touch __init__.py
-ln -s ../../tortoise-tts/tortoise/api.py api.py
-ln -s ../../tortoise-tts/tortoise/utils utils
-ln -s ../../tortoise-tts/tortoise/voices tortoise_voices
+ln -s ../tortoise-tts/tortoise tortoise
